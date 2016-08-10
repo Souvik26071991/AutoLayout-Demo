d6d9efd434a0fb8384030002c054452efdde745a
Souvik Sasmal <souvik@souviks-mac-mini.local>
2016-08-10 12:06:27 +0530

    Initial Commit

---
+++ DemoAutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DemoAutoLayout
+//
+//  Created by Souvik Sasmal on 10/08/16.
+//  Copyright © 2016 Isis Design Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DemoAutoLayout/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DemoAutoLayout
+//
+//  Created by Souvik Sasmal on 10/08/16.
+//  Copyright © 2016 Isis Design Services. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
